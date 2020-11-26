6d53d75782aac8f1f897a07db4a74f83c247c51a
João Marcos <joao.santos160@fatec.sp.gov.br>
2020-11-26 00:54:29 -0300

    Correção erro de digitação

--- database/migrations/2020_10_30_112439_criar_tabela_contatos.php
+++ database/migrations/2020_10_30_112439_criar_tabela_contatos.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CriaTabelaContatos extends Migration
+class CriarTabelaContatos extends Migration
 {
     /**
      * Run the migrations.
